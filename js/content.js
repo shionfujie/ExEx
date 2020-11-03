d85635020e4a3c8198fac623564bd233efae914b
shion.t.fujie <tonaluna32966@gmail.com>
2020-11-03 21:48:06 +0900

    extract book information

--- js/content.js
+++ js/content.js
@@ -1,3 +1,26 @@
-chrome.runtime.onMessage.addListener(message=> {
-    console.debug(message)
-})
+chrome.runtime.onMessage.addListener(message => {
+  switch (message.type) {
+    case "extract book info":
+      clipBookInformation();
+      return;
+  }
+});
+
+function clipBookInformation() {
+  const productTitle = document.getElementById("productTitle");
+  if (productTitle === undefined) {
+    console.debug("cannot find book title, perhaps not on a product page?");
+    return;
+  }
+  const contributors = Array.from(
+    document
+      .getElementById("bylineInfo")
+      .getElementsByClassName("contributorNameID")
+  );
+  console.debug(
+    contributors.map(c => c.textContent).join(" and ") +
+      ", " +
+      productTitle.textContent.replace('\n', '') +
+      document.getElementById("imgBlkFront").src
+  );
+}
